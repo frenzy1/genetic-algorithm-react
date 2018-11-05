9a26473ee05bb015caa8a13461a192565a5e11cc
Phung Nguyen <longphung.nn@gmail.com>
2018-11-05 14:41:12 +1100

    Updates

---
+++ precache-manifest.72c37139a1c7f5408640732012948ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6109b7aa08543516235e",
+    "url": "/genetic-algorithm-react/static/js/runtime~main.6109b7aa.js"
+  },
+  {
+    "revision": "fce1d22d833e2f330a49",
+    "url": "/genetic-algorithm-react/static/js/main.fce1d22d.chunk.js"
+  },
+  {
+    "revision": "a7194b2d988a5c80f583",
+    "url": "/genetic-algorithm-react/static/js/1.a7194b2d.chunk.js"
+  },
+  {
+    "revision": "fce1d22d833e2f330a49",
+    "url": "/genetic-algorithm-react/static/css/main.77bdc16d.chunk.css"
+  },
+  {
+    "revision": "58d66a8525779ae3d3d77ca2f0a5f14c",
+    "url": "/genetic-algorithm-react/index.html"
+  }
+];
